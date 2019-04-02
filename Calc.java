caf52144818f07c954a1c67275730602a3094c60
khadka <raushanKhadka@gmail.com>
2019-04-02 16:47:31 +0545

    Created main file for calculator app.

---
+++ Calc.java
@@ -0,0 +1,9 @@
+public class Calc{
+	public static void main(String[] args){
+		for(int i = 0; i<5; i++){
+			System.out.println("\n");
+		}
+
+		System.out.println("Welcome to the Calculator app.");
+	}
+}
