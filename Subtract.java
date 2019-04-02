54ec53bb0fc0bee998a83946f35645a8e47a37f5
nisha936 <nishashrestha936@gmail.com>
2019-04-02 18:21:18 +0545

    Options are added to Subtraction as well

--- Subtract.java
+++ Subtract.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class Subtract{
 	public void sub(){
 		Scanner sc = new Scanner(System.in);
+		Option op = new Option();
 		System.out.print("Enter two numbers:");
 		int firstNum= sc.nextInt();
 		int secondNum=sc.nextInt();
 		int result=firstNum - secondNum;
 		System.out.println("The difference is :"+ result);
-
+		op.chooseOption();
 	}
 }
